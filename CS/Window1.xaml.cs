2139cdce22e78e569fb221b6cf0646a6967adb86
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Collections.Generic;
+using DevExpress.Wpf.Grid;
+using DevExpress.XtraGrid;
+
+namespace WpfApplication1 {
+
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            string[] months = new string[] { "January", "February", "March", 
+                "April", "May", "June", "July", "August", "September", 
+                "October", "November", "December" };
+
+            grid.DataSource = months;
+            grid.PopulateColumns();
+            grid.SortBy(grid.Columns[0]);
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e) {
+            grid.Columns[0].SortMode = ColumnSortMode.Custom;
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e) {
+            grid.Columns[0].SortMode = ColumnSortMode.Default;
+        }
+
+        private void grid_CustomColumnSort(object sender, CustomColumnSortEventArgs e) {
+            e.Result = Comparer<int>.Default.Compare(e.ListSourceRowIndex1,
+                e.ListSourceRowIndex2);
+
+            e.Handled = true;
+        }
+    }
+}
---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,38 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Collections.Generic
+Imports DevExpress.Wpf.Grid
+Imports DevExpress.XtraGrid
+
+Namespace WpfApplication1
+
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim months() As String = { "January", "February", "March", "April", "May", "June", "July", _
+				"August", "September", "October", "November", "December" }
+
+			grid.DataSource = months
+			grid.PopulateColumns()
+			grid.SortBy(grid.Columns(0))
+		End Sub
+
+		Private Sub checkBox_Checked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			grid.Columns(0).SortMode = ColumnSortMode.Custom
+		End Sub
+
+		Private Sub checkBox_Unchecked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			grid.Columns(0).SortMode = ColumnSortMode.Default
+		End Sub
+
+		Private Sub grid_CustomColumnSort(ByVal sender As Object, ByVal e As CustomColumnSortEventArgs)
+			e.Result = Comparer(Of Integer).Default.Compare(e.ListSourceRowIndex1, e.ListSourceRowIndex2)
+
+			e.Handled = True
+		End Sub
+	End Class
+End Namespace
